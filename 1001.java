ea49d4c6c30f8248a1c60cbc38af81937ffa856d
Carlos Gabriel <carloz.sec081@gmail.com>
2026-08-26 16:09:29 -0300

    Create 1001.java

---
+++ 1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+
+       int A = read.nextInt();
+       int B = read.nextInt();
+    
+       int X = (A+B);
+       System.out.println("X = "+X);
+    }
+}
