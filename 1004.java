f6d81353e849894908459a3dfc325c81aae5b984
Carlos Gabriel <carloz.sec081@gmail.com>
2026-08-26 16:29:35 -0300

    Create 1004.java

---
+++ 1004.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+
+       int A = read.nextInt();
+       int B = read.nextInt();
+
+       int PROD = (A*B);
+       System.out.println("PROD = "+PROD);
+
+    }
+}
